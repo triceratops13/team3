f350653dbf91a8ec5f0443526c05b7774dc1ddd6
MaxTrgb <126002400+MaxTrgb@users.noreply.github.com>
2024-03-24 16:27:47 +0200

    added main logic and everything

--- clientManager/Module.cs
+++ clientManager/Module.cs
@@ -1,28 +1,85 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace clientManager
 {
-    public  class Client
+    public class ClientBuilder
     {
-        public string Name { get; set; }
-        public int id { get; private set; }        
-        public string Type { get;private set; }
+        private Client client;
 
-       private static int clientNumber = 0;
-        
-        public void generateIdNumber()
+        public ClientBuilder()
         {
-            id = clientNumber++;
+            client = new Client();
         }
 
-        public void setTypeOnline() {
-            Type = "online";
-        }
-        public void setTypeOffline()
+        public ClientBuilder WithName(string name)
         {
-            Type = "offline";
+            client.Name = name;
+            return this;
+        }
+
+        public ClientBuilder WithType(string type)
+        {
+            client.Type = type;
+            return this;
+        }
+
+        public Client Build()
+        {
+            client.GenerateIdNumber(); 
+            return client;
         }
     }
-  
+
+    public class Client
+    {
+        public string Name { get; set; }
+        public int Id { get; private set; }
+        public string Type { get; set; }
+
+        private static int clientNumber = 0;
+
+        public Client()
+        {
+            clients = new List<Client>();
+        }
+
+        private List<Client> clients;
+
+        public void GenerateIdNumber()
+        {
+            Id = clientNumber++;
+        }
+        public void AddClient(Client client)
+        {
+            clients.Add(client);
+        }
+        public void EditClient(int clientId, string newName, string newType)
+        {
+            Client client = clients.FirstOrDefault(c => c.Id == clientId);
+
+            if (client != null)
+            {
+                client.Name = newName;
+                client.Type = newType;
+            }
+            else
+            {
+                throw new InvalidOperationException("Client not found.");
+            }
+        }
+        public void RemoveClient(Client client)
+        {
+            clients.Remove(client);
+        }
+
+        public ListViewItem ToListViewItem()
+        {
+            string[] row = { Id.ToString(), Name, Type };
+            return new ListViewItem(row);
+        }
+    }
 }
